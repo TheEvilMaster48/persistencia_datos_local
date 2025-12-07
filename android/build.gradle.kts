37ec5ed3890c7cf0eb35625372ab365f000b2fe5
Santiago Domínguez <estkubiantiago16@gmail.com>
2025-12-07 09:39:27 -0500

    Actualizacion Sistema Persistencia de Datos

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,12 +5,18 @@ allprojects {
     }
 }
 
-val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    layout.buildDirectory.value(newBuildDir.dir(project.name))
-    evaluationDependsOn(":app")
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
